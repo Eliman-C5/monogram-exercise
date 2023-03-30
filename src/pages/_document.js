a1a031d6b5a893fc36abd98c2c81c676925f70dc
eli castellano <eliman1926@gmail.com>
2023-03-30 11:10:21 -0400

    Fixing title trouble in _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Front-end dev Exercise III</title>
+        <link rel="icon" href="/cubos-footer.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* crossorigin */}
+        <link rel="preconnect" href="https://fonts.gstatic.com"  />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
+      </Head>
       <body>
         <Main />
         <NextScript />
